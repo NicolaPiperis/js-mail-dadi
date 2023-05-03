164ffed1a2bf4fbf720b6d6ad7f19502831c71d4
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-03 17:51:42 +0200

    ticket test

---
+++ dadi/js/maindadi.js
@@ -0,0 +1,42 @@
+// Variabili globali
+let numberUser, generator, numberPc, esito;
+generator = document.querySelector("button");
+
+// crea una funzione sul click
+generator.addEventListener("click",
+function(){
+
+    // Chiedi all'utente un numero
+    numberUser = parseInt(document.getElementById("number").value);
+    console.log(numberUser);
+
+    // Fai scegliere al pc un numero
+    numberPc = Math.floor(Math.random() * 6) + 1;
+    console.log(numberPc);
+
+    // in base a chi ha il numero più alto decreta il vincitore
+
+    let esito = document.getElementById("answer");
+
+
+    if (numberPc > numberUser) {
+        console.log("Il vincitore è il Pc");
+        esito.innerHTML("Nessun vincitore, la partita è patta");
+    }
+
+    else if(numberPc === numberUser) {
+        console.log(" Nessun vincitore, la partita è patta");
+        esito.innerHTML("Nessun vincitore, la partita è patta");
+    }
+
+    else {
+        console.log("Il vincitore è User");
+        esito.innerHTML("Nessun vincitore, la partita è patta");
+    }
+
+    // output
+   
+}
+)
+
+
