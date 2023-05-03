4066dd6bb786525250fe5f85ff2401cb2f7177c2
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-03 17:09:26 +0200

    exercise email end

--- e-mail/js/main.js
+++ e-mail/js/main.js
@@ -12,27 +12,26 @@ generator.addEventListener("click", // Creo l'evento sul click del bottone
 function() {
 
     // Prendo il valore di ciò che è scritto nell'input e lo uso come dato dell'utente
-let emailUsers = document.querySelector("input").value;
-console.log("email : " + emailUsers);
+    let emailUsers = document.querySelector("input").value;
+    console.log("email : " + emailUsers);
+    
+    let esito;
+    esito = document.getElementById("answer");
+    esito.innerHTML = "Prova a contattare l'assistenza"
+    
 
     // fai controllare la e-mail
 for ( let i = 0; i < accesso.length; i++) {
 
-    let esito;
-    esito = document.getElementById("answer");
+    
     const emailArray = accesso[i];
     
     if ( emailUsers === emailArray) {
-        esito.innerHTML = "Benvenuto"
-        esito.classList.add("answer_green")
+        esito.innerHTML = "Benvenuto";
+        
     }
     
-    else {
-        esito.innerHTML = "Prova a contattare l'assistenza"
-        esito.classList.add("answer_red")
-    }
-
-    }
+}
 
 
 
