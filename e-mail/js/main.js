03d99d7435412919e0a19da18a50153e7efabdb7
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-03 15:33:30 +0200

    email html, css and js

---
+++ e-mail/js/main.js
@@ -0,0 +1,32 @@
+// Variabili globali
+let generator, emailUsers;
+
+generator = document.querySelector("button");
+
+// Creare una lista array
+const accesso = ["nicolapiperis9999@gmail.com", "piperisnicola9999@gmail.com"];
+
+// Chiedi all'utente la e-mail attraverso un input e bottone 
+
+generator.addEventListener("click", // Creo l'evento sul click del bottone
+function() {
+
+    // Prendo il valore di ciò che è scritto nell'input e lo uso come dato dell'utente
+let emailUsers = document.querySelector("input").value;
+console.log("email : " + emailUsers);
+
+    // fai controllare la e-mail
+if ( emailUsers = accesso) {
+    console.log("Benvenuto");
+}
+
+else {
+    console.log("Hai problemi di accesso? Prova a contattare l'assistenza");
+}
+
+    // output di risposta in basa all'esito del controllo
+
+
+}
+)
+
